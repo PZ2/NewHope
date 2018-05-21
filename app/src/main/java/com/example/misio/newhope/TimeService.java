7f0f7fb3166c205cd2026ced31c219909775cce9
febicen <szczukowski.damian@gmail.com>
2018-05-21 21:57:30 +0200

    pulsy nie będą już ujemne

--- app/src/main/java/com/example/misio/newhope/TimeService.java
+++ app/src/main/java/com/example/misio/newhope/TimeService.java
@@ -131,26 +131,28 @@ import io.realm.RealmResults;
         @Override
         public void onNotification(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
             UUID alertUUID = characteristic.getUuid();
-            Log.d("odczyt pulsu", " - odczyt pulsu: " + Arrays.toString(characteristic.getValue()));
+            Log.d("odczyt pulsu", " - odczyt pulsu: " + characteristic.getValue()[1]);
 
-            final RealmPulseReading pulse = new RealmPulseReading();
-            Calendar calendar = Calendar.getInstance();
-            java.util.Date now = calendar.getTime();
-            long x = now.getTime();
-            pulse.setDate(x);
-            pulse.setValue((int)(characteristic.getValue()[1]));
+            if (characteristic.getValue()[1] >= 0){
+                final RealmPulseReading pulse = new RealmPulseReading();
+                Calendar calendar = Calendar.getInstance();
+                java.util.Date now = calendar.getTime();
+                long x = now.getTime();
+                pulse.setDate(x);
+                pulse.setValue((int)(characteristic.getValue()[1]));
 
-            Realm.init(this);
-            RealmConfiguration realmConfiguration = new RealmConfiguration.Builder().build();
-            Realm realm = Realm.getInstance(realmConfiguration);
+                Realm.init(this);
+                RealmConfiguration realmConfiguration = new RealmConfiguration.Builder().build();
+                Realm realm = Realm.getInstance(realmConfiguration);
 
-            realm.executeTransaction(new Realm.Transaction() {
-                @Override
-                public void execute(Realm realm) {
-                    realm.insertOrUpdate(pulse);
-                }
-            });
-            lifeCheck();
+                realm.executeTransaction(new Realm.Transaction() {
+                    @Override
+                    public void execute(Realm realm) {
+                        realm.insertOrUpdate(pulse);
+                    }
+                });
+                lifeCheck();
+            }
         }
 
         public void sendSms()
